c5f05ad25486ec3db4d4227782fa3dc0742c5fac
tim peterson <trpeters1@hotmail.com>
2014-02-23 07:32:32 -0500

    added V2 methods

--- lib/dogeapi.php
+++ lib/dogeapi.php
@@ -22,11 +22,16 @@ class DogeAPI
      * cURL GET request driver
      */
 
-    private function _request($method, $path, $args = array(), $api_key=true)
+    private function _request($method, $path, $args = array(), $api_key=true, $api_version='v1')
     {
+        
+        // Check API version
+        if($api_version=='v1') $uri2='';
+        else $uri2='/'.$api_version.'/';
+        
         // Generate cURL URL
-        if($api_key==true) $url =  'https://www.dogeapi.com/wow/?api_key=' . $this->api_key . $path;
-        else $url =  'https://www.dogeapi.com/wow/?a=' . $path;
+        if($api_key==true) $url =  'https://www.dogeapi.com/wow/'.$uri2.'?api_key=' . $this->api_key . $path;
+        else $url =  'https://www.dogeapi.com/wow/'.$uri2.'?a=' . $path;
 
         // Check for args and build query string
         if (!empty($args)) {
@@ -133,5 +138,89 @@ class DogeAPI
     {
         return $this->_request('GET', '&a=get_current_price');
     } 
+
+
+//New actions in API V2 are below
+//https://www.dogeapi.com/api_documentation_v2
+
     
+    //Creates a new user identified by {USER_ID} and returns their payment address. Each user has only one payment address.
+    // /wow/v2/?api_key={API_KEY}&a=create_user&user_id={USER_ID}
+    public function create_user($args = array(), $api_version='v2')
+    {
+        return $this->_request('GET', '&a=create_user&user_id=', $args);
+    } 
+
+
+    //get_user_address
+    //Returns the payment address assigned to the user with a given {USER_ID}
+    // /wow/v2/?api_key={API_KEY}&a=get_user_address&user_id={USER_ID}
+    public function get_user_address($args = array(), $api_version='v2')
+    {
+        return $this->_request('GET', '&a=get_user_address', $args);
+    } 
+
+
+
+//get_user_balance
+//Returns the balance of the user with a given {USER_ID}.
+// /wow/v2/?api_key={API_KEY}&a=get_user_balance&user_id={USER_ID}
+    public function get_user_balance($args = array(), $api_version='v2')
+    {
+        return $this->_request('GET', '&a=get_user_balance', $args);
+    } 
+
+
+// withdraw_from_user
+// Withdraws {AMOUNT_DOGE} from {USER_ID} to {PAYMENT_ADDRESS}. Requires your {PIN}
+// /wow/v2/?api_key={API_KEY}&a=withdraw_from_user&user_id={USER_ID}&pin={PIN}&amount_doge={AMOUNT_DOGE}&payment_address={PAYMENT_ADDRESS}
+ 
+    public function withdraw_from_user($args = array(), $api_version='v2')
+    {
+        return $this->_request('GET', '&a=withdraw_from_user', $args);
+    } 
+
+
+// move_to_user
+// Moves {AMOUNT_DOGE} to user with ID {TO_USER_ID} from user with ID {FROM_USER_ID}. There is no network fee for this transaction, just the DogeAPI fee.
+// /wow/v2/?api_key={API_KEY}&a=move_to_user&to_user_id={TO_USER_ID}&from_user_id={FROM_USER_ID}&amount_doge={AMOUNT_DOGE}
+
+    public function move_to_user($args = array(), $api_version='v2')
+    {
+        return $this->_request('GET', '&a=move_to_user', $args);
+    } 
+
+
+//get_users
+//Returns a list of users asssociated with your account with their balances.
+// /wow/v2/?api_key={API_KEY}&a=get_users
+    public function get_users($args = array(), $api_version='v2')
+    {
+        return $this->_request('GET', '&a=get_users', $args);
+    } 
+
+//get_transactions
+// Returns a list of the {NUMBER} most recent transactions matching the options. {USER_ID}, {PAYMENT_ADDRESS}, {LABEL}, and {TYPE} are all optional. The types are receive, send, move, and fee.
+// /wow/v2/?api_key={API_KEY}&a=get_transactions&num={NUMBER}&type={TYPE}
+    public function get_transactions($args = array(), $api_version='v2')
+    {
+        return $this->_request('GET', '&a=get_transactions', $args);
+    } 
+
+//get_network_hashrate
+//Returns the current network hashrate. This doesn't require an API key.
+///wow/v2/?a=get_network_hashrate
+    public function get_network_hashrate($api_key=false, $api_version='v2')
+    {
+        return $this->_request('GET', '&a=get_network_hashrate');
+    } 
+
+//get_info
+//Returns current information, including price in USD/BTC, block count, difficulty, 5 minute price change, network hashrate, and API version. This doesn't require an API key.
+///wow/v2/?a=get_info 
+    public function get_info($api_key=false, $api_version='v2')
+    {
+        return $this->_request('GET', '&a=get_info');
+    }
+
 }
